b9162437d0db270a39ea8d5174ae4ddd81666a70
Vittorio Lo Mele <vittorio@mrbackslash.it>
2021-03-13 16:20:09 +0100

    WIP: sendon and sendoff completed (NO TEST)

--- src/main/java/it/mrbackslash/discordspigotstatus/DiscordSender.java
+++ src/main/java/it/mrbackslash/discordspigotstatus/DiscordSender.java
@@ -2,32 +2,63 @@ package it.mrbackslash.discordspigotstatus;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 
 public class DiscordSender {
-    private String pip, pwebhook;
+    private String pwebhook, serverOnFormatted, serverOffFormatted;
     DiscordSender(String ip, String webhook){
-        pip = ip;
+        Unirest.setTimeouts(0, 0);
         pwebhook = webhook;
+        //read templates
+        File son = new File(getClass().getResource("server-on.json").getFile());
+        File soff = new File(getClass().getResource("server-off.json").getFile());
+        try{
+            String serverOn = FileUtils.readFileToString(son, StandardCharsets.UTF_8);
+            String serverOff = FileUtils.readFileToString(soff, StandardCharsets.UTF_8);
+            serverOnFormatted = String.format(serverOn, ip);
+            serverOffFormatted = String.format(serverOff, ip);
+        }catch (Exception e){}
     }
 
+
+
     public boolean sendOn(){
-        Unirest.setTimeouts(0, 0);
         try{
-            //TODO: finish sendon!
             HttpResponse<String> response = Unirest.post(
-                    ""
+                    pwebhook
             )
                     .header("Content-Type", "application/json")
-                    .body("")
+                    .body(serverOnFormatted)
                     .asString();
-            return true;
+            //TODO: NOT TESTED
+            if(response.toString().length() == 0){
+                return true;
+            }else {
+                return false;
+            }
         }catch (Exception e){
             return false;
         }
     }
 
     public boolean sendOff(){
-        //TODO: Finish sendoff
-        return true;
+        try{
+            HttpResponse<String> response = Unirest.post(
+                    pwebhook
+            )
+                    .header("Content-Type", "application/json")
+                    .body(serverOffFormatted)
+                    .asString();
+            if(response.toString().length() == 0){
+                return true;
+            }else {
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
     }
 }
